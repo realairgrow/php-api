4c6e3053df386595affa572446f56569b22fc3a0
ALEXANDER BOIKOV <alexanderboikov@ALEXANDERs-MacBook.local>
2019-03-20 12:15:16 -0700

    Added composer and airgrow.php

---
+++ airgrow.php
@@ -0,0 +1,58 @@
+<?php
+
+class Airgrow {
+    function __construct($product, $key){
+        $this->product = $product;
+        $this->key = $key;
+
+        $authorization = $this->post([
+            "product" => $this->product,
+            "key" => $this->key,
+            "action" => "login" 
+        ], "https://tracker.airgrow.com/collect.php");
+
+        $this->authorized = ($authorization["status"] == "success");
+
+        if ($this->authorized)
+            $this->tracker = $authorization["tracker"];
+    }
+
+    function event($name, $parameters = []){
+        $data = [
+            "product" => $this->product,
+            "key" => $this->key,
+            "action" => "custom",
+            "id" => "api:" . $name
+        ];
+
+        foreach ($parameters as $key => $value) 
+            $data[$key] = $value;
+        
+        return $this->post($data);
+    }
+
+    function post($data, $url = NULL){
+        $curl = curl_init();
+
+        if ($url == NULL){
+            if (!$this->authorized)
+                return false;
+
+            $url = "https://" . $this->tracker . "/event";
+        }
+
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return (array)json_decode("[" . $result . "]")[0];
+    }   
+};
+
+?>
